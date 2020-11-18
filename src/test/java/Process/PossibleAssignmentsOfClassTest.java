e07292590dab2aaa0d18b1370a806a1e56cd0248
georgianna <georgiannamak@gmail.com>
2020-11-18 20:50:32 +0200

    Refactoring methods using streams

---
+++ src/test/java/Process/PossibleAssignmentsOfClassTest.java
@@ -0,0 +1,14 @@
+package Process;
+
+import junit.framework.TestCase;
+import Problem.Class;
+
+public class PossibleAssignmentsOfClassTest extends TestCase {
+
+    public void testAssignRandomRoomAndTime() {
+
+    }
+
+    public void testFindRandomRoomforClass() {
+    }
+}
